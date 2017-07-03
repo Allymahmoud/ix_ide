f9b916682fb23789aafdbff2986dcdb75509c1bc
Ally Mahmoud <ally.h.mahmoud.19@dartmouth.edu>
2017-07-03 14:55:49 +0200

    Initial Commit

---
+++ ix_ide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ix_ide
+//
+//  Created by Ally Mahmoud on 7/3/17.
+//  Copyright © 2017 Ally Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
